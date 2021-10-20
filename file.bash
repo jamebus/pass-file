cb4c622bb22432516c65f39cbacda6149352b6b6
James Kelly <james@hicag.org>
2021-10-20 16:23:29 -0500

    Reformat file.bash

--- file.bash
+++ file.bash
@@ -1,129 +1,128 @@
 #!/usr/bin/env bash
 
 print_usage() {
-    echo "Usage: $PROGRAM file action pass-name [path]"
-    echo "Actions:"
-    echo "  store|add|attach: add new file to password store"
-    echo "  retrieve|show|cat: retrieve file from password store and print it to stdout"
-    echo "  edit|vi: edit a file (warning: unencrypted file will be opened with \$EDITOR)"
-    exit 0
+	echo "Usage: $PROGRAM file action pass-name [path]"
+	echo "Actions:"
+	echo "  store|add|attach: add new file to password store"
+	echo "  retrieve|show|cat: retrieve file from password store and print it to stdout"
+	echo "  edit|vi: edit a file (warning: unencrypted file will be opened with \$EDITOR)"
+	exit 0
 }
 
 cmd_store() {
-    local path="$1"
-    local file="$2"
+	local path="$1"
+	local file="$2"
 
-    if [[ ${path: -4} != ".b64" ]]; then
-	path="${path}.b64"
-    fi
+	if [[ ${path: -4} != ".b64" ]]; then
+		path="${path}.b64"
+	fi
 
-    local passfile="$PREFIX/$path.gpg"
+	local passfile="$PREFIX/$path.gpg"
 
-    cd $OLDPWD # fix for relative paths
+	cd $OLDPWD # fix for relative paths
 
-    check_sneaky_paths "$1"
-    set_git "$passfile"
+	check_sneaky_paths "$1"
+	set_git "$passfile"
 
-    if [[ -z $path || -z $file ]]; then
-        print_usage
-    elif [[ ! -f $file ]]; then
-        die "Error: $file does not exist."
-    fi
+	if [[ -z $path || -z $file ]]; then
+		print_usage
+	elif [[ ! -f $file ]]; then
+		die "Error: $file does not exist."
+	fi
 
-    if [[ -f $passfile ]] && [[ "$PASS_FILE_FORCE_OVERWRITE" != "true" ]]; then
-        read -r -p "A file with this name already exists in the store. Do you want to overwrite it? [y/N] " response
-        if [[ $response != [yY] ]]; then
-            exit 0;
-        fi
-    fi
+	if [[ -f $passfile ]] && [[ "$PASS_FILE_FORCE_OVERWRITE" != "true" ]]; then
+		read -r -p "A file with this name already exists in the store. Do you want to overwrite it? [y/N] " response
+		if [[ $response != [yY] ]]; then
+			exit 0
+		fi
+	fi
 
-    mkdir -p "$(dirname "$passfile")"
+	mkdir -p "$(dirname "$passfile")"
 
-    set_gpg_recipients "$(dirname "$path")"
+	set_gpg_recipients "$(dirname "$path")"
 
-    base64 $file | $GPG -e "${GPG_RECIPIENT_ARGS[@]}" -o "$passfile" "${GPG_OPTS[@]}" 
+	base64 $file | $GPG -e "${GPG_RECIPIENT_ARGS[@]}" -o "$passfile" "${GPG_OPTS[@]}"
 
-    git_add_file $passfile "Store arbitary file for $path to store."
+	git_add_file $passfile "Store arbitary file for $path to store."
 }
 
 cmd_retrieve() {
-    local path="$1"
+	local path="$1"
 
-    if [[ ${path: -4} != ".b64" ]]; then
-	path="${path}.b64"
-    fi
+	if [[ ${path: -4} != ".b64" ]]; then
+		path="${path}.b64"
+	fi
 
-    local passfile="$PREFIX/$path.gpg"
+	local passfile="$PREFIX/$path.gpg"
 
-    if [[ -z $path ]]; then 
-        print_usage
-    else
-        check_sneaky_paths "$path"
-        $GPG -d "${GPG_OPTS[@]}" "$passfile" | base64 -d || exit $?
-    fi
+	if [[ -z $path ]]; then
+		print_usage
+	else
+		check_sneaky_paths "$path"
+		$GPG -d "${GPG_OPTS[@]}" "$passfile" | base64 -d || exit $?
+	fi
 }
 
 cmd_edit() {
-    local path="$1"
+	local path="$1"
 
-    if [[ -z $path ]]; then 
-        print_usage
-    fi
+	if [[ -z $path ]]; then
+		print_usage
+	fi
 
-    if [[ ${path: -4} != ".b64" ]]; then
-	path="${path}.b64"
-    fi
+	if [[ ${path: -4} != ".b64" ]]; then
+		path="${path}.b64"
+	fi
 
-    local passfile="$PREFIX/$path.gpg"
+	local passfile="$PREFIX/$path.gpg"
 
-    if [[ -z $EDITOR ]]; then
-	echo "\$EDITOR not set, don't know how to open file."
-	exit 1
-    else
-	local tmpfile=$(mktemp)
-	local newfile=0
-	chmod 0600 $tmpfile
-	
-	if [[ -f $passfile ]]; then
-		cmd_retrieve $path > $tmpfile
+	if [[ -z $EDITOR ]]; then
+		echo "\$EDITOR not set, don't know how to open file."
+		exit 1
+	else
+		local tmpfile=$(mktemp)
+		local newfile=0
+		chmod 0600 $tmpfile
+
+		if [[ -f $passfile ]]; then
+			cmd_retrieve $path >$tmpfile
+			if [[ $? -ne 0 ]]; then
+				rm $tmpfile
+				exit 1
+			fi
+		else
+			echo "File does not exist, creating new file..."
+			sleep 3
+		fi
+
+		$EDITOR $tmpfile
 		if [[ $? -ne 0 ]]; then
 			rm $tmpfile
 			exit 1
 		fi
-	else
-		echo "File does not exist, creating new file..."
-		sleep 3
-	fi
 
-	$EDITOR $tmpfile
-	if [[ $? -ne 0 ]]; then
+		PASS_FILE_FORCE_OVERWRITE="true" cmd_store $path $tmpfile
+		if [[ $? -ne 0 ]]; then
+			echo "Could not save file, please check yourself."
+			echo "Tempfile: ${tmpfile}"
+			exit 1
+		fi
+
 		rm $tmpfile
-		exit 1
 	fi
-
-	PASS_FILE_FORCE_OVERWRITE="true" cmd_store $path $tmpfile
-	if [[ $? -ne 0 ]]; then
-		echo "Could not save file, please check yourself."
-		echo "Tempfile: ${tmpfile}"
-		exit 1
-	fi
-
-	rm $tmpfile
-    fi
 }
 
 case $1 in
-    store|add|attach)
-        shift && cmd_store "$@"
-        ;;
-    retrieve|show|cat)
-        shift && cmd_retrieve "$@"
-        ;;
-    edit|vi)
+store | add | attach)
+	shift && cmd_store "$@"
+	;;
+retrieve | show | cat)
+	shift && cmd_retrieve "$@"
+	;;
+edit | vi)
 	shift && cmd_edit "$@"
 	;;
-    *)
-        print_usage
-        ;;
+*)
+	print_usage
+	;;
 esac
-
